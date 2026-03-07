e888df5b540435071283b3500fd11fe8d5c3d48d
david <david@mac.home>
2026-03-07 16:32:40 +0100

    add flyonUI

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,9 +6,17 @@ export interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ title }) => {
   return (
-    <div className="header-container">
-      <input type="text" placeholder="Type here" className="input" />
-    </div>
+    <header className="mb-10 flex justify-center flex-col items-center">
+      <h1 className="text-3xl mb-5">{title}</h1>
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="input input-sm w-64 bg-white/90 hover:bg-white focus:bg-white focus:outline-none focus:ring-2 focus:ring-white/50 transition-all placeholder:text-gray-500 pr-10"
+        />
+       
+      </div>
+    </header>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flyonui/dist/js/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("flyonui")
+  ],
 }
